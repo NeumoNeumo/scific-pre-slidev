6ae1f7d52e392b6b14d41e6638d80beb31f8b3fc
Jianhui Yuan Xu <neuromoments@gmail.com>
2023-04-04 23:30:25 +0800

    add github Actions

--- assets/PresenterPrint-4376ccf0.js
+++ assets/PresenterPrint-4376ccf0.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as h,u as p,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as i,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-c594504e.js";import{N as V}from"./NoteDisplay-81fed53e.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(h),p(`
+import{d as _,i as d,a as h,u as p,b as u,c as m,e as f,f as n,g as t,t as s,h as a,F as g,r as v,n as x,j as y,o as i,k as b,l as N,m as k,p as P,q as w,_ as S}from"./index-2c42011b.js";import{N as V}from"./NoteDisplay-6785685c.js";const j={class:"m-4"},L={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(h),p(`
 @page {
   size: A4;
   margin-top: 1.5cm;
